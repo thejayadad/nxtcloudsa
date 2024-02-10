4d91f5c70d2c516576086417997afb166ac17350
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-02-10 15:47:37 -0500

    update

---
+++ myapp/lib/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        if(mongoose.connections[0].readyState){
+            return true
+        }
+
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("MongoDB Connected")
+    } catch (error) {
+        console.log("error")
+    }
+}
+
+export default connectDB;
